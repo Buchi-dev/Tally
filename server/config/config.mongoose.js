2274affef60d013c3d4de1e62024692b7eea4a0a
Buchi-dev <tjyuzon02@gmail.com>
2025-09-17 13:57:05 +0800

    Update MongoDB connection string to use Atlas URI

--- server/config/config.mongoose.js
+++ server/config/config.mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     // Use MongoDB Atlas connection string or local MongoDB
-    const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/tally_survey';
+    const mongoURI = 'mongodb+srv://hedtjyuzon_db_user:cGXDkzcnt8NDmUAt@cluster0.cz31cp2.mongodb.net/';
     
     await mongoose.connect(mongoURI);
     
